de1b8074b6743a722648414b8d8fe70eb55e30c1
MiaDev4 <ndeyeamyciss2@gmail.cocm>
2026-01-04 00:20:38 +0000

    introduction de ejs

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
 # Ndeye_Amy_Ciss_TP_Nodejs
 
- Ceci est une API REST pour gérer les produits d'un petit magasin.
-L'application permettra d'ajouter, consulter, modifier et supprimer des produits.
+Ceci est une API REST pour gérer les produits d'un petit magasin.
+L'application permettra d'ajouter, consulter, modifier et supprimer des produits via une interface utilisatuer.
 
- Tout le code est écrit dans un seul fichier server.js.
+ Tout le code est écrit dans un seul fichier server.js et les vues dans un dossier views/.
  
 J'ai eu à :
  - Créer un projet Node.js avec npm init -y.
  - Installer les dépendances : express, mysql2, nodemon.
  - Configurer le script dev dans package.json pour lancer nodemon.
-
+ - Configurer EJS comme moteur de template.
+ - Créer une base de données magasin_db(pour sauvegarder mes donnéés) dans magasin.sql.
+ - Le serveur écoute sur le port 3000.
+ - Afficher un tableau HTML avec tous les produits.
+ - Bouton pour la modification (lien vers modifier.ejs).
+ - Bouton pour supprimer les produits de quantité 0.
+ - Inclure une barre de navigation avec liens vers : 
+     . Accueil (liste des produits)
+     . Ajouter un produit
+     . Rechercher
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const mysql = require("mysql2");
 
 const app = express();
 app.use(express.json());
+app.set("view engine", "ejs");
+app.set("views", "views");
+app.use(express.urlencoded({ extended: true }));
 
 const db = mysql.createConnection({
     host: "localhost",
@@ -19,38 +22,50 @@ db.connect((err) => {
     }
 });
 
+//Affichage de la liste des produits
+app.get("/produits", (req, res) => {
+    db.query("SELECT * FROM produits", (err, result) => {
+        if (err) return res.status(500).send( "Erreur SQL");
+        res.render("index.ejs", { produits: result });
+    });
+});
+//Fin affichage de la liste des produits
+
+//Ajout d'un produit
+
+app.get("/ajouter", (req, res) => {
+  res.render("ajouter", { erreur: null, data: {} });
+});
+
 app.post("/produits", (req, res) => {
     const { nom, prix, quantite, categorie } = req.body;
 
     if (!nom || prix === undefined || quantite === undefined || !categorie) {
-        return res.status(400).send( "Tous les champs sont obligatoires." );
+        return res.status(400).render( "ajouter", { erreur: "Tous les champs sont obligatoires.", data: req.body });
     }
 
+    if (prix <= 0 && quantite < 0) {
+        return res.status(400).render("ajouter", { erreur: "Le prix doit être > 0 et la quantité ne peut pas être négative.", data: req.body });
+    }
     if (prix <= 0) {
-        return res.status(400).send( "Le prix doit être > 0" );
+        return res.status(400).render("ajouter", { erreur: "Le prix doit être > 0", data: req.body });
     }
 
     if (quantite < 0) {
-        return res.status(400).send( "La quantité ne peut pas être négative." );
+        return res.status(400).render("ajouter", { erreur: "La quantité ne peut pas être négative.", data: req.body });
     }
 
     const sql = "INSERT INTO produits (nom, prix, quantite, categorie) VALUES (?, ?, ?, ?)";
 
     db.query(sql, [nom, prix, quantite, categorie], (err, result) => {
-        if (err) return res.status(500).send( "Erreur SQL" );
+        if (err) return res.status(500).send("Erreur SQL" );
 
-        res.json( "Produit ajouté avec succès");
-    });
+        res.redirect("/produits");});
 });
+//Fin ajout d'un produit
 
-app.get("/produits", (req, res) => {
-    db.query("SELECT * FROM produits", (err, result) => {
-        if (err) return res.status(500).send( "Erreur SQL");
-        res.json(result);
-    });
-});
-
-app.get("/produits/recherche", (req, res) => {
+//Recherche de produits
+app.get("/recherche", (req, res) => {
     let { categorie, prix_min, prix_max, en_stock } = req.query;
 
     let sql = "SELECT * FROM produits WHERE 1=1";
@@ -78,30 +93,38 @@ app.get("/produits/recherche", (req, res) => {
 
     db.query(sql, params, (err, result) => {
         if (err) return res.status(500).send( "Erreur SQL" );
-        res.json(result);
-    });
+        res.render("recherche", { produit: result , query: req.query });
+});
+});
+//Fin recherche de produits
+
+//Modification d'un produit
+app.get("/modifier/:id", (req, res) => {
+  db.query(
+    "SELECT * FROM produits WHERE id = ?",
+    [req.params.id],
+    (err, rows) => {
+      if (rows.length === 0) return res.status(404).send("Produit introuvable");
+      res.render("modifier", { produit: rows[0], erreur: null });
+    }
+  );
 });
 
-app.get("/produits/:id", (req, res) => {
-    const sql = "SELECT * FROM produits WHERE id = ?";
-    db.query(sql, [req.params.id], (err, result) => {
-        if (err) return res.status(500).send( "Erreur SQL" );
-
-        if (result.length === 0) {
-            return res.status(404).send( "Produit non trouvé" );
-        }
-
-        res.json(result[0]);
-    });
-});
-
-
-app.put("/produits/:id", (req, res) => {
+app.post("/produits/:id/modifier", (req, res) => {
     const { nom, prix, quantite, categorie } = req.body;
 
     if (!nom || prix === undefined || quantite === undefined || !categorie) {
-        return res.status(400).send( "Tous les champs sont obligatoires." );
+        return res.status(400).render("modifier", { erreur: "Tous les champs sont obligatoires.", produit:{...req.body, id:req.params.id} });
     }
+    if (prix <= 0 && quantite < 0) {
+        return res.status(400).render("modifier", { erreur: "Le prix doit être > 0 et la quantité ne peut pas être négative.", produit:{...req.body,id: req.params.id} });
+    }
+    if (prix <= 0) {
+        return res.status(400).render("modifier", { erreur: "Le prix doit être > 0", produit:{...req.body,id: req.params.id} });
+    }
+    if (quantite < 0) {
+        return res.status(400).render("modifier", { erreur: "La quantité ne peut pas être négative.", produit:{...req.body,id: req.params.id} });
+    }   
 
     const sql = `UPDATE produits SET 
         nom=?,
@@ -113,15 +136,14 @@ app.put("/produits/:id", (req, res) => {
     db.query(sql, [nom, prix, quantite, categorie, req.params.id], (err, result) => {
         if (err) return res.status(500).send( "Erreur SQL" );
 
-        if (result.affectedRows === 0) {
-            return res.status(404).send("Produit non trouvé" );
-        }
-
-        res.send("Produit modifié avec succès" );
+      res.redirect("/produits");
     });
 });
+//Fin modification d'un produit
 
-app.delete("/produits/:id", (req, res) => {
+//Suppression d'un produit
+
+app.post("/produits/:id/supprimer", (req, res) => {
     const id = req.params.id;
 
     db.query("SELECT quantite FROM produits WHERE id=?", [id], (err, result) => {
@@ -131,15 +153,15 @@ app.delete("/produits/:id", (req, res) => {
             return res.status(404).send( "Produit non trouvé" );
 
         if (result[0].quantite > 0)
-            return res.status(400).send( "Suppression interdite quantité est supérieure à 0" );
+            return res.status(400).render("supprimer", { produit: result[0] });
 
         db.query("DELETE FROM produits WHERE id=?", [id], (err2, resultat) => {
             if (err2) return res.status(500).send( "Erreur SQL" );
 
-            res.send( "Produit supprimé avec succès" );
-        });
+            res.redirect("/produits");});
     });
 });
+//Fin suppression d'un produit
 
 app.listen(3000, () => {
     console.log("Serveur démarré sur le port 3000");
